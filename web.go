b74541b6bd1bc9dbae153dbf4672fe5dc43cba79
Joe Gillotti <joe@u13.net>
2017-09-16 11:31:08 -0700

    go fmt

--- web.go
+++ web.go
@@ -13,37 +13,37 @@ var staticPath string
 var mimes map[string]string
 
 func SinglePageApp(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  path := staticPath + "/spa.html"
-  contents, err := ioutil.ReadFile(path)
-  if err == nil {
-    fmt.Fprint(w, string(contents))
-  } else {
-    fmt.Fprint(w, "failed loading html page")
-    log.Printf("Failed loading %v: %v", path, err)
-  }
+	path := staticPath + "/spa.html"
+	contents, err := ioutil.ReadFile(path)
+	if err == nil {
+		fmt.Fprint(w, string(contents))
+	} else {
+		fmt.Fprint(w, "failed loading html page")
+		log.Printf("Failed loading %v: %v", path, err)
+	}
 }
 
 func StaticResource(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-  path := staticPath + ps.ByName("path")
-  ext:= filepath.Ext(path)
-  mime, _ := mimes[ext]
-  contents, err := ioutil.ReadFile(path)
-  if err == nil {
-    if mime != "" {
-      w.Header().Set("content-type", mime)
-    }
-    fmt.Fprint(w, string(contents))
-  } else {
-    fmt.Fprint(w, "failed loading file")
-    log.Printf("Failed loading %v: %v", path, err)
-  }
+	path := staticPath + ps.ByName("path")
+	ext := filepath.Ext(path)
+	mime, _ := mimes[ext]
+	contents, err := ioutil.ReadFile(path)
+	if err == nil {
+		if mime != "" {
+			w.Header().Set("content-type", mime)
+		}
+		fmt.Fprint(w, string(contents))
+	} else {
+		fmt.Fprint(w, "failed loading file")
+		log.Printf("Failed loading %v: %v", path, err)
+	}
 }
 
 func Web(r *redis.Client) {
-  staticPath = "static"
+	staticPath = "static"
 	mimes = map[string]string{}
-  mimes[".css"] = "text/css"
-  mimes[".js"] = "text/javascript"
+	mimes[".css"] = "text/css"
+	mimes[".js"] = "text/javascript"
 
 	router := httprouter.New()
 	router.GET("/", SinglePageApp)
