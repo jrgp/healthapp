8abaf0713e4f033eb14c3e7c90ff39d1855b0345
Joe Gillotti <joe@u13.net>
2017-09-16 11:38:24 -0700

    stub out needed routes

--- web.go
+++ web.go
@@ -9,8 +9,15 @@ import "github.com/julienschmidt/httprouter"
 import "github.com/go-redis/redis"
 
 var staticPath string
-
 var mimes map[string]string
+
+func init() {
+	staticPath = "static"
+	mimes = map[string]string{}
+	mimes[".css"] = "text/css"
+	mimes[".js"] = "text/javascript"
+	mimes[".png"] = "image/png"
+}
 
 func SinglePageApp(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	path := staticPath + "/spa.html"
@@ -39,14 +46,35 @@ func StaticResource(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 	}
 }
 
-func Web(r *redis.Client) {
-	staticPath = "static"
-	mimes = map[string]string{}
-	mimes[".css"] = "text/css"
-	mimes[".js"] = "text/javascript"
+func GetServerStatus(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
 
+func PostServerStatus(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
+
+func ServersList(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
+
+func AlertsList(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
+
+func AlertInfo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
+
+func Web(r *redis.Client) {
 	router := httprouter.New()
+
+	// Backend api routes
+	router.GET("/api/v0/status/:server_name", GetServerStatus)
+	router.POST("/api/v0/status/:server_name", PostServerStatus)
+
+	router.GET("/api/v0/servers", ServersList)
+	router.GET("/api/v0/alerts", AlertsList)
+	router.GET("/api/v0/alert/:alert_id", AlertInfo)
+
+	// UI routes
 	router.GET("/", SinglePageApp)
 	router.GET("/static/*path", StaticResource)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
