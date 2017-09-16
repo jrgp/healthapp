7155cc536a24bc7488dd8f773c121fca2180d36f
Joe Gillotti <joe@u13.net>
2017-09-16 11:01:36 -0700

    add config file loading
    same syntax/options as python impl

---
+++ config.go
@@ -0,0 +1,60 @@
+package main
+
+import "log"
+import "os"
+import "fmt"
+import "io/ioutil"
+import  "gopkg.in/yaml.v2"
+
+type Config struct {
+  Redis string `yaml:"redis"`
+  ApiKey string `yaml:"api_key"`
+  ApiURL string `yaml:"api_url"`
+  SetUID string `yaml:"setuid"`
+  SetGID string `yaml:"setgid"`
+  ServerStalenessDuration int `yaml:"server_staleness_duration"`
+  AlertProcessInterval int `yaml:"alert_process_interval"`
+  AlertSendEmailInterval int `yaml:"alert_send_email_interval"`
+  EnableEmails bool `yaml:"enable_emails"`
+  EmailServer string `yaml:"email_server"`
+  EmailSender string `yaml:"email_sender"`
+  EmailRecipients []string `yaml:"email_recipients"`
+}
+
+var Configs Config
+
+func init () {
+  paths := []string{
+      os.Getenv("CONFIG_FILE"),
+     "/etc/healthapp/config.yaml",
+     "/usr/local/healthapp/config.yaml",
+     "config.yaml",
+  }
+
+  var contents []byte
+  contents = nil
+
+  for _, path := range paths {
+    if path == "" {
+      continue
+    }
+
+    this_contents, err := ioutil.ReadFile(path)
+
+    if err == nil {
+      contents = this_contents
+      fmt.Println("Reading config from", path)
+      break
+    }
+  }
+
+  if contents == nil {
+    fmt.Println("Could not find any config files to load. Try setting CONFIG_FILE to its path")
+    os.Exit(1)
+  }
+
+  err := yaml.Unmarshal(contents, &Configs)
+  if err != nil {
+    log.Fatalf("error: %v", err)
+  }
+}
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import "os"
 import "flag"
 import "github.com/go-redis/redis"
 
-
 func get_redis() *redis.Client {
   return redis.NewClient(&redis.Options{
     Addr:     "127.0.0.1:6379",
